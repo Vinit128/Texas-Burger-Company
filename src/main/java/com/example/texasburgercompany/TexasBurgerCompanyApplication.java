4aec2d770f2168424f0be7fac4ce389114e50857
VINIT PATEL <patelvinit128@gmail.com>
2021-07-05 17:19:20 -0500

    Added Swagger

--- src/main/java/com/example/texasburgercompany/TexasBurgerCompanyApplication.java
+++ src/main/java/com/example/texasburgercompany/TexasBurgerCompanyApplication.java
@@ -1,14 +1,14 @@
 package com.example.texasburgercompany;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableWebSecurity
-
 public class TexasBurgerCompanyApplication {
 
     public static void main(String[] args) {
