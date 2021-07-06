f388ff44ae3278792c02753489390a5d605c853b
VINIT PATEL <patelvinit128@gmail.com>
2021-07-05 23:02:39 -0500

    Added Exceptions and swagger

---
+++ src/main/java/com/example/texasburgercompany/Exception/MenuNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.texasburgercompany.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(code= HttpStatus.NOT_FOUND)
+public class MenuNotFoundException extends RuntimeException{
+
+    public MenuNotFoundException(String message)
+    {
+        super(message);
+    }
+}
